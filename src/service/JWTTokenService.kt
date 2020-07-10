8a735a6c41b86b694dec08042ed96933687ba7ec
Arsenko <minsenka@gmail.com>
2020-07-10 05:20:43 +0300

    inc token life

--- src/service/JWTTokenService.kt
+++ src/service/JWTTokenService.kt
@@ -11,6 +11,6 @@ public class JWTTokenService {
 
     fun generate(id: Int): String = JWT.create()
         .withClaim("id", id)
-        .withExpiresAt(Date(System.currentTimeMillis() + 600000))//1час 40минут
+        .withExpiresAt(Date(System.currentTimeMillis() + 6000000))//1час 40минут
         .sign(algo)
 }
